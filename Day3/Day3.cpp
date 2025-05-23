2c6f516313afd567180f8703172b473ae08ada09
kingdiesel <aspivak@gmail.com>
2025-05-22 19:54:41 -0500

    day 3 completed

--- Day3/Day3.cpp
+++ Day3/Day3.cpp
@@ -24,8 +24,6 @@ int main()
 	}
 
 	std::regex re(R"(mul\((\d+),(\d+)\))");
-	std::vector<std::pair<int, int>> mul_pairs;
-
 	auto it = std::sregex_iterator(input_as_line.begin(), input_as_line.end(), re);
 	auto end = std::sregex_iterator();
 
@@ -34,10 +32,33 @@ int main()
 	{
 		int a = std::stoi((*it)[1].str());
 		int b = std::stoi((*it)[2].str());
-		mul_pairs.emplace_back(a, b);
 
 		sum += a * b;
 	}
 
 	std::cout << sum << std::endl;
+
+	std::regex re2(R"(do\(\).*?don't\(\))");
+	std::vector<std::string> groups;
+
+	for (auto it = std::sregex_iterator(input_as_line.begin(), input_as_line.end(), re2); it != std::sregex_iterator(); ++it)
+	{
+		groups.push_back(it->str());
+	}
+
+	int sum2 = 0;
+	for (std::size_t i = 0; i < groups.size(); ++i)
+	{
+		auto it = std::sregex_iterator(groups[i].begin(), groups[i].end(), re);
+		auto end = std::sregex_iterator();
+
+		for (; it != end; ++it)
+		{
+			int a = std::stoi((*it)[1].str());
+			int b = std::stoi((*it)[2].str());
+			sum2 += a * b;
+		}
+	}
+
+	std::cout << sum2 << std::endl;
 }
---
+++ Day4/Day4.cpp
@@ -0,0 +1,7 @@
+https://adventofcode.com/2024/day/4
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
