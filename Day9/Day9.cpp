fb81fccbfcd1bb1232a6aa8875097ed25b75dfe8
kingdiesel <aspivak@gmail.com>
2025-05-26 12:53:40 -0500

    part 2 setup

--- Day9/Day9.cpp
+++ Day9/Day9.cpp
@@ -7,7 +7,7 @@
 #include <string>
 #include <cassert>
 #include <cstdlib>
-//#define SAMPLE_INPUT
+#define SAMPLE_INPUT
 
 struct FileBlock
 {
@@ -18,6 +18,13 @@ struct FileBlock
 	{
 		return is_file ? std::to_string(file_id) : ".";
 	}
+};
+
+struct FileDescriptor
+{
+	int starting_index = 0;
+	int size = 0;
+	int file_id = 0;
 };
 
 void Compress(std::vector<FileBlock>& file_blocks)
@@ -45,12 +52,13 @@ int64_t GetChecksum(std::vector<FileBlock>& file_blocks)
 		{
 			checksum += static_cast<int64_t>(i) * static_cast<int64_t>(file_blocks[i].file_id);
 		}
-		else
-		{
-			break;
-		}
 	}
 	return checksum;
+}
+
+void Compress2(std::vector<FileBlock>& file_blocks, std::map<int, std::vector<int>>& free_space_to_index, std::vector<FileDescriptor> descriptors)
+{
+
 }
 
 int main()
@@ -94,7 +102,7 @@ int main()
 			id++;
 		}
 	}
-	constexpr bool print = false;
+	constexpr bool print = true;
 	if (print)
 	{
 		for (const auto& block : file_blocks)
@@ -105,8 +113,45 @@ int main()
 		std::cout << std::endl;
 	}
 
-	Compress(file_blocks);
+	std::map<int, std::vector<int>> free_space_to_index;
+	std::vector<FileDescriptor> file_descriptors;
+	for (int i = 0; i < file_blocks.size(); ++i)
+	{
+		if (!file_blocks[i].is_file)
+		{
+			int free_start = i;
+			for (int j = free_start; j < file_blocks.size(); ++j)
+			{
+				if (file_blocks[j].is_file)
+				{
+					int free_size = j - free_start;
+					if (free_space_to_index.find(free_size) == free_space_to_index.end())
+					{
+						free_space_to_index[free_size] = std::vector<int>();
+					}
+					free_space_to_index[free_size].push_back(free_start);
+					i = j-1;
+					break;
+				}
+			}
+		}
+		else
+		{
+			int file_size = 0;
+			int file_start = i;
+			int current_file_id = file_blocks[i].file_id;
+			while (i < file_blocks.size() && file_blocks[i].file_id == current_file_id)
+			{
+				file_size++;
+				i++;
+			}
+			file_descriptors.emplace_back(FileDescriptor{ file_start, file_size, current_file_id });
+			i--; // Adjust for the outer loop increment
+		}
+	}
 
+	//Compress(file_blocks);
+	Compress2(file_blocks, free_space_to_index, file_descriptors);
 	if (print)
 	{
 		for (const auto& block : file_blocks)
@@ -117,6 +162,5 @@ int main()
 	}
 
 	std::cout << "Checksum: " << GetChecksum(file_blocks) << std::endl;
-
 	return 0;
 }
