a4d1ea5354ad397a4d1ce9b17d17fc293b2a1a39
kingdiesel <aspivak@gmail.com>
2025-05-29 06:39:49 -0500

    setting up input for day 10

--- Day10/Day10.cpp
+++ Day10/Day10.cpp
@@ -1,8 +1,71 @@
 // https://adventofcode.com/2024/day/10
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cassert>
 
+#define SAMPLE_INPUT
+
+#ifdef SAMPLE_INPUT
+constexpr int rows = 4;
+constexpr int columns = 4;
+#else
+constexpr int rows = 54;
+constexpr int columns = 54;
+#endif
+
+static int grid[rows][columns] = {};
+struct GridPoint
+{
+	GridPoint(int in_row, int in_column)
+		: row(in_row), column(in_column) {}
+
+	GridPoint() = default;
+
+	bool IsValid() const
+	{
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+
+	// call IsValid before using this function
+	int GetValue() const
+	{
+		assert(IsValid());
+		return grid[row][column];
+	}
+
+	bool operator==(const GridPoint& other) const
+	{
+		return row == other.row && column == other.column;
+	}
+
+	int row = 0;
+	int column = 0;
+};
 int main()
 {
-    std::cout << "Hello World!\n";
+#ifdef SAMPLE_INPUT
+	std::ifstream input_file("sample_input.txt");
+#else
+	std::ifstream input_file("input.txt");
+#endif
+	if (!input_file)
+	{
+		std::cerr << "Cannot open file.";
+		return 1;
+	}
+
+	std::string line;
+	int row = 0;
+	while (std::getline(input_file, line))
+	{
+		for (int i = 0; i < line.length(); i++)
+		{
+			grid[row][i] = line[i] - '0';
+		}
+		row++;
+	}
     return 0;
 }
