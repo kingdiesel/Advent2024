56400fb3028c45b03e344ed997cd028d495279fa
kingdiesel <aspivak@gmail.com>
2025-06-03 07:47:49 -0500

    day 14 setup

---
+++ Day14/Day14.cpp
@@ -0,0 +1,26 @@
+// https://adventofcode.com/2024/day/14
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cassert>
+#include <map>
+#include <regex>
+#define SAMPLE_INPUT
+
+int main()
+{
+#ifdef SAMPLE_INPUT
+    std::ifstream input_file("sample_input.txt");
+#else
+    std::ifstream input_file("input.txt");
+#endif
+    if (!input_file)
+    {
+        std::cerr << "Cannot open file.";
+        return 1;
+    }
+
+    return 0;
+}
